4ef95f65fe8ee132248c986ae7c2db3f172b1ce5
Ketlen Miranda <costaketlen46@gmail.com>
2026-08-19 17:37:57 -0300

    Configurar variavel de tema e fonte

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="bg-[#913FDD] w-full px-8 py-1 justify-end items-center flex">
+    <header className="bg-primary w-full px-8 py-1 justify-end items-center flex">
       <nav className="flex items-center gap-6">
         <a href="#sobre-mim" className="text-white font-medium text-sm">Sobre Mim</a>
         <a href="#habilidades" className="text-white font-medium text-sm">Habilidades</a>
